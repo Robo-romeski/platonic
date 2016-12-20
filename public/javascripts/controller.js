b47630cc41cb813fe2945b9139b369349f332b6a
System Administrator <root@Ks-Mac-mini.home>
2016-12-19 23:02:23 -0500

    asset name simplification

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -6,3 +6,19 @@ fz.controller('checkCtrl', ['$scope', function ($scope) {
 	console.log('controller loaded');
 	
 }]);
+
+fz.controller('vidBgCtrl', ['$scope', function ($scope) {
+
+	$scope.resources = [
+			'./videos/vid1.mp4',
+			'./videos/vid2.mp4',
+			'./videos/vid3.mp4'
+	],
+			//$scope.poster = './images/dribble/flare.png',
+			$scope.fullScreen = false,
+			$scope.muted = true,
+			$scope.zIndex = '22'
+			$scope.pausePlay = true,
+			$scope.currentResourceIdx = 0;
+			$scope.playInfo = {};
+}])
