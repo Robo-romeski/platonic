7c49ebfcb0dc4abf905c0c9f70f4fc46c5f80347
System Administrator <root@Ks-Mac-mini.home>
2016-12-28 07:44:38 -0500

    static html

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -9,16 +9,15 @@ fz.controller('checkCtrl', ['$scope', function ($scope) {
 
 fz.controller('vidBgCtrl', ['$scope', function ($scope) {
 
-	$scope.resources = [
-			'./videos/vid1.mp4',
-			'./videos/vid2.mp4',
-			'./videos/vid3.mp4'
-	],
+$scope.resources = ['./videos/vid1.mp4'];
+$scope.features = [	'./videos/vid2.mp4'];
+$scope.alternate = ['./videos/vid3.mp4'];
+	
 			//$scope.poster = './images/dribble/flare.png',
 			$scope.fullScreen = false,
 			$scope.muted = true,
 			$scope.zIndex = '22'
-			$scope.pausePlay = true,
+			$scope.pausePlay = false,
 			$scope.currentResourceIdx = 0;
 			$scope.playInfo = {};
 }])
