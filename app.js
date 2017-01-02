15389052782f2468d8cdf49dc4cdb33344170f32
System Administrator <root@Ks-Mac-mini.home>
2017-01-01 20:39:29 -0500

    first login attempt

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+require('rootpath')();
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressJwt = require('express-jwt');
+var config = require('config.json');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -11,9 +15,32 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname + '/public/views/'));
 app.use(express.static(__dirname + '/public'));
 //app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+//login essentials
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({ secret: config.secret, resave: false, saveUninitialized: true }));
+
+// use JWT auth to secure the api
+app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate', '/api/users/register'] }));
+
+// routes
+// app.use('/login', require('./controllers/login.controller'));
+// app.use('/register', require('./controllers/register.controller'));
+// app.use('/app', require('./controllers/app.controller'));
+// app.use('/api/users', require('./controllers/api/users.controller'));
+
+// make '/app' default route
+app.get('/', function (req, res) {
+    return res.redirect('/app');
+});
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
