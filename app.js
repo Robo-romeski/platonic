3ced586cf4a63f2f199795dfe3a2ccd96d91f83a
System Administrator <root@Ks-Mac-mini.home>
2017-01-16 23:16:56 -0500

    login system 302

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var config = require('config.json');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var register = require('./routes/register');
+//var users = require('./routes/users');
 
 var app = express();
 
@@ -20,10 +22,14 @@ app.set('views', path.join(__dirname + '/public/views/'));
 app.use(express.static(__dirname + '/public'));
 // app.engine('html', require('ejs').renderFile);
 // app.set('view engine', 'html');
-app.get('*',function(req,res){
-	console.log(__dirname)
-	res.sendFile(path.join(__dirname + '/public/index.html'));
-});
+
+
+// app.get('*',function(req,res){
+// 	console.log(__dirname)
+// 	res.sendFile(path.join(__dirname + '/public/index.html'));
+// });
+
+
 //app.set('view engine', 'jade');
 
 app.engine('html', require('ejs').renderFile);
@@ -39,14 +45,14 @@ app.use(session({ secret: config.secret, resave: false, saveUninitialized: true 
 app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate', '/api/users/register'] }));
 
 // routes
-// app.use('/login', require('./controllers/login.controller'));
-// app.use('/register', require('./controllers/register.controller'));
-// app.use('/app', require('./controllers/app.controller'));
-// app.use('/api/users', require('./controllers/api/users.controller'));
+app.use('/login', require('./controllers/login.controller'));
+app.use('/register', require('./controllers/register.controller'));
+app.use('/app', require('./controllers/app.controller'));
+app.use('/api/users', require('./controllers/api/users.controller'));
 
 // make '/app' default route
 app.get('/', function (req, res) {
-    return res.redirect('/app');
+    return res.redirect('/public');
 });
 
 
@@ -66,8 +72,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+//app.use('/', index);
 app.use('/users', users);
+app.use('/register', register);
+
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
